7647a6eb092683cdd3356e4c31261572b0898192
Deepak Venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2025-09-05 13:09:15 -0700

    Update ex02.lisp

--- Chapter 02/ex02.lisp
+++ Chapter 02/ex02.lisp
@@ -1,1 +1,26 @@
-;; Chapter 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;                    ;;;;;;;;;;;;;;;;
+;;;;;;;;;;      Chapter 02    ;;;;;;;;;;;;;;;;
+;;;;;;;;;;                    ;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+					; 10 evaluates to 10
+(+ 2 3)
+					; "hello, world" evaluates to "hello, world"
+
+(format t "hello, world")
+
+(defun hello-world ()
+  (format t "hello, world"))
+
+(load (compile-file "pclworkbook.lisp"))
+; compiling file "/Users/deepak/programming/PCL/pclworkbook.lisp" (written 05 SEP 2025 12:26:39 PM):
+
+; wrote /Users/deepak/programming/PCL/pclworkbook.fasl
+; compilation finished in 0:00:00.049
+hello, world
+T
+
