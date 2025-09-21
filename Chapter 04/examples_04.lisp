31cc3c96267041532cfa1b5eb7f911e33f4b4734
Deepak Venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2025-09-21 06:12:56 -0700

    Update examples_04.lisp

--- Chapter 04/examples_04.lisp
+++ Chapter 04/examples_04.lisp
@@ -7,3 +7,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(defun hello-world ()
+  (format t "hello, world"))
+
+(defun print-list (list)
+  (dolist (i list)
+    (format t "item: ~a~%" i)))
+
+;;;; File heading comment
+;;;  Paragraph/large code section
+;;   Applies to code that follows
+;    Applies to a single line
+
